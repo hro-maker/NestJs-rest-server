ec5a7cf3fffc9b2df52fbbd5e9d7a20b4deb3151
hro-maker <hrant.muradyan.1998@mail.ru>
2021-05-04 10:44:51 +0400

    some

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Appmodule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(Appmodule);
   const PORT=process.env.PORT || 3000
+    app.enableCors()
   await app.listen(PORT,()=>{
     console.log(`server startet on port ${PORT}`)
   });
